5b61d9bb212c82956d814a2e38b36e5e1546c908
Babin Bohora <bohorababin1@gmail.com>
2022-05-31 20:27:54 +0900

    Router added and App refactored with Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,18 @@
 import './App.css';
-import { useState } from "react"
-
+// import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from './components/Login';
+import Register from './components/Register';
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  async function registerUser(event) {
-    event.preventDefault();
-    const response =  await fetch("http://localhost:1234/api/register",{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },  
-      body: JSON.stringify({
-        name,
-        email,
-        password
-      }),
-    })
-    const data = await response.json();
-    console.log(data);
-  }
-
 
   return (
     <div className="App">
-      <h1>
-        Register
-      </h1>
-      <form onSubmit={registerUser}>
-        <input type="text" placeholder="name" onChange={()=> setName(this.value)} /><br/>
-        <input type="email" placeholder="email" onChange={()=> setEmail(this.value)} />
-        <br/>
-        <input type="password" placeholder="password" onChange={()=> setPassword(this.value)} />
-        <input type="submit" value="Submit" />
-      </form>
+      <Router>
+        <Routes>
+          <Route path="/login" exact element={ <Login /> }/>
+          <Route path="/register" exact element={ <Register /> }/>
+        </Routes>
+      </Router>
     </div>
   );
 }
