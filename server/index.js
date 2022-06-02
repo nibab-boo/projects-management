3a68e0cc9b83ffa5358b3b87b12237266319dad1
Babin Bohora <bohorababin1@gmail.com>
2022-06-02 12:59:01 +0900

    bcrypt package added and index refactored for decrypt

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require('dotenv').config({ path: './../.env' });
 const User = require("./models/user.model");
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcryptjs');
 
 
 app.use(cors());
@@ -22,11 +22,13 @@ mongoose.connect( url)
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
   try {
+    // BCRYPT USER BEFORE CREATING IT.
+    const newPassword = await bcrypt.hash(req.body.password);
     // CREATE USER
     await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: newPassword
     })
     // res.send(user);
     res.json({status: "Ok"})
@@ -40,10 +42,13 @@ app.post("/api/register", async (req, res) => {
 app.post("/api/login", async (req, res) => {
   console.log(req.body);
   // CHECK IF USER EXISTS OR NOT
-  const user = await User.findOne({ email: req.body.email, password: req.body.password})
+  const user = await User.findOne({ email: req.body.email})
+
   
   if (user) {
+    const isValidPassword = await bcrypt.compare(req.body.password, user.password )
     //  CREATING TOKEN FOR SESSION
+    if (!isValidPassword) return res.json({status: "error", user: "invalid password"})
     const token = jwt.sign(
       {
         name: user.name,
@@ -54,6 +59,48 @@ app.post("/api/login", async (req, res) => {
     return res.json({status: "Ok", user: token})
   } else {
     return  res.json({status: "error", user: false})
+  }
+
+})
+
+app.get("/api/login", async (req, res) => {
+  // console.log("Hello");
+  const token = req.headers['x-access-token'];
+  console.log(token);
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    console.log(decoded);
+    const email = decoded.email;
+    const user = await User.findOne(
+      { email: email }
+    );
+
+    res.json({ status: "Ok", quote: user.quote })
+  } catch(err) {
+    console.log(err);
+    res.json({ status: "error", error: "invalid token" })
+  }
+
+})
+app.post("/api/quote", async (req, res) => {
+  // console.log("Hello");
+  const token = req.headers['x-access-token'];
+  console.log(token);
+  console.log(req.body)
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    const email = decoded.email;
+    console.log(email);
+    const user = await User.updateOne(
+      { email: email },
+      { $set: { quote: req.body.quote } }  
+    );
+    console.log(user.quote);
+
+    res.json({ status: "Ok", quote: user.quote })
+  } catch(err) {
+    console.log(err);
+    res.json({ status: "error", error: "invalid token" })
   }
 
 })
