acd52c24491abc73e8f68debaf0514bd8ed41c63
Babin Bohora <bohorababin1@gmail.com>
2022-06-02 14:19:08 +0900

    projects subdocument added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
+const Project = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    details: { type: String, required: true },
+    urlLink: { type: String, unique: true },
+    repoLink: { type: String, unique: true },
+    status: { type: String },
+    stacks: [String],
+    hosting: { type: String, required: true }
+  }
+)
+
 const User = new mongoose.Schema(
   {
-    name: { type: String, required: true},
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-    quote: {type: String},
+    name: { type: String, required: true },
+    email: { type: String, required: true , unique: true },
+    password: { type: String, required: true },
+    quote: { type: String },
+    projects: [Project]
   },
   { collection: 'user-data'}
 )
