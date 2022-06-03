4ca96a1feb632f8feeb8cd7155779634eba314a6
Babin Bohora <bohorababin1@gmail.com>
2022-06-04 02:49:09 +0900

    RootPage created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-// import { useState } from "react"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import Login from './components/Login';
-import Register from './components/Register';
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import RootPage from './components/RootPage';
 import Dashboard from './components/Dashboard';
 import Quote from './components/Quote';
 import ProjectForm from './components/ProjectForm';
 
 function App() {
+
+
 
   return (
     <div className="App">
@@ -15,18 +16,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={ 
             <>
-            <div className='container'>
-              <div>
-                <h3> Projects Management App</h3>
-                <p className='mt-3'>
-                  <strong>Record your app ideas and working project in a organized way</strong>
-                </p>
-              </div>
-              <div className='d-flex justify-content-evenly p-5 my-5 shadow bg-body rounded'>
-                <Register />
-                <Login />
-              </div>
-            </div>
+              <RootPage />
             </>
           }/>
           <Route path="/dashboard" exact element={ <Dashboard /> }/>
---
+++ client/src/components/RootPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+
+const RootPage = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  React.useEffect(() => {
+    if (token) {
+      navigate("/dashboard");
+    }
+  })
+  return (
+    <div className='container'>
+    <div>
+      <h3> Projects Management App</h3>
+      <p className='mt-3'>
+        <strong>Record your app ideas and working project in a organized way</strong>
+      </p>
+    </div>
+    <div className='d-flex justify-content-evenly p-5 my-5 shadow bg-body rounded'>
+      <Register />
+      <Login />
+    </div>
+  </div>
+  );
+};
+
+export default RootPage;
