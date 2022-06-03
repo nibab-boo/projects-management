381a0a1326bf53f57042ed42db40afc00b411e67
Babin Bohora <bohorababin1@gmail.com>
2022-06-03 21:20:35 +0900

    done

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ const { Console } = require('console');
 const PORT = process.env.PORT || 1234
 
 if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
   // Exprees will serve up production assets
-  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build', 'index.html')));
+  app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
