c29bd3d3b68940f5c43d250bb0f7b3e74d6fab8e
Babin Bohora <bohorababin1@gmail.com>
2022-06-03 19:35:18 +0900

    refactored index.js to serve static site to production

--- server/index.js
+++ server/index.js
@@ -12,6 +12,18 @@ const { Console } = require('console');
 
 const PORT = process.env.PORT || 1234
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 app.use(cors());
 
 // GOING TO USE JSON
