f2232a0f20ac760d0b08a3e805abcb8a786f50c5
Babin Bohora <bohorababin1@gmail.com>
2022-06-03 20:08:17 +0900

    link added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 // import { useState } from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import Quote from './components/Quote';
+
 function App() {
 
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" exact element={ 
+            <>
+              <div>
+                <Link to="/login">LOGIN</Link>
+                <Link to="/register">register</Link>
+                <Link to="/dashboard">dashboard</Link>
+              </div>
+            </>
+          }/>
+
           <Route path="/login" exact element={ <Login /> }/>
           <Route path="/register" exact element={ <Register /> }/>
           <Route path="/dashboard" exact element={ <Dashboard /> }/>
