5c3666f480573ab1de4bae579ce3bc53cef0e69c
Babin Bohora <bohorababin1@gmail.com>
2022-06-07 15:53:32 +0900

    basic test for text written

--- client/src/tests/RootPage.test.tsx
+++ client/src/tests/RootPage.test.tsx
@@ -1,8 +1,14 @@
+
 import { render, within, screen } from '@testing-library/react';
-import App from '../App';
+import RootPage from '../components/RootPage';
+import { StaticRouter } from "react-router-dom/server";
 
 test('renders learn react link', async () => {
-  render(<App />);
+  render(
+    <StaticRouter location={"/"}>
+      <RootPage />
+    </StaticRouter>
+    );
 
   const header = within( await screen.findByTestId("main-title")).getByText("Projects Management App");
 
